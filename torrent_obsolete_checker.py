cb7e7d747c4f5c2c91e4aae3ebba91da7e3f2e23
jfaustt <jfaust@tutanota.com>
2018-06-26 00:01:34 +1200

    Initial commit

---
+++ torrent_obsolete_checker.py
@@ -0,0 +1,21 @@
+from qbittorrent import Client
+
+if __name__ == '__main__':
+	qb = Client('http://127.0.0.1:8080/')
+
+	torrents = qb.torrents()
+	count = 0
+
+	for t in torrents:
+		t_trackers = qb.get_torrent_trackers(t['hash'])
+
+		for tt in t_trackers:
+			if tt['status'] == 'Not working':
+				if 'This torrent does not exist' in tt['msg']:
+					print('AnimeBytes - ' + t['name'])
+					count += 1
+				elif 'Unregistered torrent' in tt['msg']:
+					print('Other      - ' + t['name'])
+					count += 1
+
+	print('\n' + str(count) + ' obsolete torrents found!')
